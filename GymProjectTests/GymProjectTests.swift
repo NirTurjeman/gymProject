b08cc5cedcec908384867cf2c781b6805c242d47
NirTurjeman <nirturjeman@gmail.com>
2025-06-02 11:20:16 +0300

    Initial Commit

---
+++ GymProjectTests/GymProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  GymProjectTests.swift
+//  GymProjectTests
+//
+//  Created by NirTurjeman on 02/06/2025.
+//
+
+import Testing
+@testable import GymProject
+
+struct GymProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
